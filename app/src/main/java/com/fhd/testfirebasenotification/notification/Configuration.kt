8b65f60e38e9d5a254fb0d551d0cb355cd7117f1
sanketchheladiya <mychance2001@gmail.com>
2022-02-05 17:15:54 +0530

    first commit

---
+++ app/src/main/java/com/fhd/testfirebasenotification/notification/Configuration.kt
@@ -0,0 +1,16 @@
+package com.fhd.testfirebasenotification.notification
+
+class Configuration {
+
+    companion object {
+        val TOPIC_GLOBAL = "testnotification"
+
+        val REGISTRATION_COMPLETE = "registrationComplete"
+        val PUSH_NOTIFICATION = "pushNotification"
+        const val NOTIFICATION_ID = 100
+        const val NOTIFICATION_ID_BIG_IMAGE = 101
+
+        val SHARED_PREF = "ah_firebase"
+    }
+
+}
